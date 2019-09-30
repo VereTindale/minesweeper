1b2232d6766835e09d2ca98c9681e66ae3ae563f
vere tindale <veretindale@gmail.com>
2019-09-30 16:34:15 +1300

    ermegerd its a board!!!

--- minesweeper.js
+++ minesweeper.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
-
+var board = {
+  cells: [
+    
+    {row:0, col:0, isMine: Boolean, hidden: true},
+    {row:0, col:1, isMine: Boolean, hidden: true},
+    {row:0, col:2, isMine: Boolean, hidden: true},
+    {row:0, col:3, isMine: Boolean, hidden: true},
+    {row:1, col:0, isMine: Boolean, hidden: true},
+    {row:1, col:1, isMine: Boolean, hidden: true},
+    {row:1, col:2, isMine: Boolean, hidden: true},
+    {row:1, col:3, isMine: Boolean, hidden: true},
+    {row:2, col:0, isMine: Boolean, hidden: true},
+    {row:2, col:1, isMine: Boolean, hidden: true},
+    {row:2, col:2, isMine: Boolean, hidden: true},
+    {row:2, col:3, isMine: Boolean, hidden: true},
+    {row:3, col:0, isMine: Boolean, hidden: true},
+    {row:3, col:1, isMine: Boolean, hidden: true},
+    {row:3, col:2, isMine: Boolean, hidden: true},
+    {row:3, col:3, isMine: Boolean, hidden: true}
+    ]
+  }
 function startGame () {
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
